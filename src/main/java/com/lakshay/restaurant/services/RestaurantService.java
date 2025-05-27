0be3e788babe8615a686c8f3e842e190d0b914b2
lakshaybxt <bishtlakshay14@gmail.com>
2025-05-27 08:45:47 +0530

    Added Get point of find by Id

--- src/main/java/com/lakshay/restaurant/services/RestaurantService.java
+++ src/main/java/com/lakshay/restaurant/services/RestaurantService.java
@@ -5,6 +5,8 @@ import com.lakshay.restaurant.domain.entities.Restaurant;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 public interface RestaurantService {
     Restaurant createRestaurant(RestaurantCreateUpdateRequest request);
     Page<Restaurant> searchRestaurants(
@@ -15,4 +17,5 @@ public interface RestaurantService {
             Float radius,
             Pageable pageable
     );
+    Optional<Restaurant> getRestaurant(String id);
 }
