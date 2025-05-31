4ee267896f6f4d22ba20b2c62241666cdecfa099
lakshaybxt <bishtlakshay14@gmail.com>
2025-05-31 09:03:29 +0530

    Added review controller

--- src/main/java/com/lakshay/restaurant/controllers/ReviewController.java
+++ src/main/java/com/lakshay/restaurant/controllers/ReviewController.java
@@ -9,6 +9,10 @@ import com.lakshay.restaurant.mappers.ReviewMapper;
 import com.lakshay.restaurant.services.ReviewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -34,6 +38,31 @@ public class ReviewController {
         Review createdReview =  reviewService.createReview(user, restaurantId, reviewCreateUpdateRequest);
 
         return ResponseEntity.ok(reviewMapper.toReviewDto(createdReview));
+    }
+
+    @GetMapping
+    public Page<ReviewDto> listReviews(
+            @PathVariable String restaurantId,
+            @PageableDefault(
+                    size = 20,
+                    page = 0,
+                    sort = "datePosted",
+                    direction = Sort.Direction.DESC) Pageable pageable
+            ) {
+        return reviewService
+                .listReviews(restaurantId, pageable)
+                .map(reviewMapper::toReviewDto);
+    }
+
+    @GetMapping(path = "/{reviewId}")
+    public ResponseEntity<ReviewDto> getReview(
+            @PathVariable String restaurantId,
+            @PathVariable String reviewId
+    ) {
+        return reviewService.getReview(restaurantId, reviewId)
+                .map(reviewMapper::toReviewDto)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.noContent().build());
     }
 
     private User jwtToUser(Jwt jwt) {
--- src/main/java/com/lakshay/restaurant/services/ReviewService.java
+++ src/main/java/com/lakshay/restaurant/services/ReviewService.java
@@ -3,7 +3,14 @@ package com.lakshay.restaurant.services;
 import com.lakshay.restaurant.domain.ReviewCreateUpdateRequest;
 import com.lakshay.restaurant.domain.entities.Review;
 import com.lakshay.restaurant.domain.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 public interface ReviewService {
     Review createReview(User author, String restaurantId, ReviewCreateUpdateRequest review);
+    Page<Review> listReviews(String restaurantId, Pageable pageable);
+    Optional<Review> getReview(String restaurantId, String reviewId);
+    Review updateReview(User author, String restaurantId, String reviewId, ReviewCreateUpdateRequest review);
 }
